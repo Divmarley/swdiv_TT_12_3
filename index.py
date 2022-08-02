f4a9e1dc2a98b800d747c4a054e43322caf28210
Divmarley <nexted.marley@gmail.com>
2022-08-02 13:45:32 +0000

    first commit

---
+++ index.py
@@ -0,0 +1,43 @@
+# variableName  = value/data
+ 
+
+# PersonAge = 20
+name = '''thi si the was i want 
+to do in this class sleep and talk'''
+print(type(name))
+print(name)
+# print(type(mySchoolname))
+PersonAge,mySchoolname ='20', "opl"
+# print(PersonAge)
+# print(type(mySchoolname))
+
+# fruit = "kiwi"
+# print(fruit)
+# age = 90j
+# height = 2.7
+# print(type(age))
+# print(type(height))
+
+
+
+# isMale = True
+# print(type(isMale))
+
+
+# friut = ['kiwi','mango','apple']
+# # print(type(friut))
+# print(friut)
+
+
+allDataTypes =[
+    True,
+    False,
+    500,
+    5.00,
+    6j,
+    "fdjfhdk",
+    ['kiwi','mango','apple']
+]
+
+print(allDataTypes[-3])
+# print(type(allDataTypes[-1]))
